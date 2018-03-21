c37e9ec1dc5fa5ca1dd6c186faf6a651ab9df642
Tim Hyson <tim.hyson@wonga.com>
2018-03-21 13:35:01 +0000

    Add nav bar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  wedding = { name: 'our wedding' };
+  menuItems: MenuItem[];
+
+  constructor() {
+    this.sidenavActions = new EventEmitter<any>();
+    this.sidenavParams = [];
+
+    this.menuItems = [
+          { name: "Our Story", route: "/our-story" },
+          { name: "When & where", route: "/when-where" }
+      ];
+  }
+
+  close() {
+    this.sidenavActions.emit({ action: 'sideNav', params: ['hide'] });
+  }
+
+  sidenavActions: EventEmitter<any>;
+  sidenavParams: any[];
+}
+
+export interface MenuItem {
+
+  name: string;
+  route: string;
 }
