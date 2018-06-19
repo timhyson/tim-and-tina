444f62eedda37814f0b6c6ca318c89da9bdcc070
Tim Hyson <tim.hyson@wonga.com>
2018-06-19 22:26:23 +0100

    Add video trailer to gallery page.

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-gallery',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
+  dangerousVideoUrl: string;
+  videoUrl: SafeResourceUrl;
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) {
+    this.updateVideoUrl('VCNNxglhlwA');
+  }
 
   ngOnInit() {
   }
 
+  updateVideoUrl(id: string) {
+    // Appending an ID to a YouTube URL is safe.
+    // Always make sure to construct SafeValue objects as close as possible
+    // to the input data so that it's easier to check if the value is safe.
+    this.dangerousVideoUrl = `https://www.youtube.com/embed/${id}` +
+      `?rel=0&amp;controls=0&amp;showinfo=0&amp;modestbranding=1`;
+    this.videoUrl =
+      this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousVideoUrl);
+  }
 }
